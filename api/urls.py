606d8b05b50dfc326a659d87dd25501dda8972b2
91d906h4 <fairymanning@gmail.com>
2022-09-15 22:19:21 +0900

    update

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<str:id>', views.get, name="Index"),
+    path('<str:key>/<str:id>', views.get, name="Index"),
 ]
--- api/views.py
+++ api/views.py
@@ -3,6 +3,8 @@ from .functions.get_weather import *
 
 # Create your views here.
 
-def get(request, id):
-    data = get_weather_data(id) # C0A770 466950
-    return JsonResponse(data, safe=False, json_dumps_params={'ensure_ascii': False})
+def get(request, key, id):
+    if int(key) % 104729 == 0:
+        data = get_weather_data(id) # C0A770 466950
+        return JsonResponse(data, safe=False, json_dumps_params={'ensure_ascii': False})
+    else: return JsonResponse({"status": "error", "message": "Incorrect API key."}, safe=False, json_dumps_params={'ensure_ascii': False})
